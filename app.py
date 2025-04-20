c5f9e00022793a95436b19eeba0ef2bdeb773b16
ChaimakamFSS <kammounchaima1@fss.u-sfax.tn>
2025-04-20 11:41:02 +0100

    Ajustement du nombre de recommandations

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = FastAPI()
 
 class RecommandationRequest(BaseModel):
     id_etudiant: int
+    top_n: int = 5  # Nombre de recommandations à renvoyer
 
 @app.post("/recommander/")
 def recommander_api(request: RecommandationRequest):
@@ -17,7 +18,7 @@ def recommander_api(request: RecommandationRequest):
 
 @app.post("/recommander_svd/")
 def recommander_svd_api(request: RecommandationRequest):
-    result = recommander_svd(request.id_etudiant)
+    result = recommander_svd(request.id_etudiant, top_n=request.top_n)
     if result is None:
         return {"error": "Étudiant non trouvé"}
     return result
